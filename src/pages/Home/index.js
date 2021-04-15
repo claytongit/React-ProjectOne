bba769c47955737712d12e55acf2729625ca44af
claytontav <claytontavares61@gmail.com>
2021-04-15 19:39:02 -0300

    primeiro commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,111 +4,89 @@ import Posts from '../../components/Posts';
 import Button from '../../components/Button';
 import TextInput from '../../components/TextInput';
 
-import { Component } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { loadPosts } from '../../utils/load-posts';
 
-class Home extends Component{
+export default function Home(){
 
-  state = {
-    posts: [], //Estado dos posts
-    allPosts: [], //Estados de todos os posts para paginação
-    page: 0, //Paginas que iram ser renderizados ao carregar a pagina
-    postsPerPage: 2, //Quantidade de paginas que ira carregar ao clicar no botao
-    searchValue: '' //Valor inicial do campo de pesquisa
-  };
+  const [posts, setPosts] = useState([]);  //Estado dos posts
+  const [allPosts, setAllPosts] = useState([]); //Estados de todos os posts para paginação
+  const [page, setPage] = useState(0); //Paginas que iram ser renderizados ao carregar a pagina
+  const [postsPerPage] = useState(2); //Quantidade de paginas que ira carregar ao clicar no botao
+  const [searchValue, setSearchValue] = useState(''); //Valor inicial do campo de pesquisa
 
-  async componentDidMount(){
-    await this.loadPosts();
-  }
+  const noMorePosts = page + postsPerPage >= allPosts.length; //Desativar botao se tiver acabado os posts
+
+  const filterPosts = !!searchValue 
+  ? 
+    allPosts.filter(post => { //Filtra todos os posts
+      //Transforma todos os titulos em letra minuscula, includes incluir os searchValue em letra minusculas
+      return post.title.toLowerCase().includes(searchValue.toLowerCase());
+    })
+  : 
+    posts
+  ;  
 
   //Função para carregar os posts
-  loadPosts = async () => {
-
-    const { page, postsPerPage } = this.state;
+  const handleLoadPosts = useCallback( async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
 
-    this.setState({ 
-      posts: postsAndPhotos.slice(page, postsPerPage), 
-      allPosts: postsAndPhotos 
-    });
-
-  }
+  useEffect(()=>{
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
 
   //Função de paginação
-  loadMorePosts = () => {
-
-    const {
-      page,
-      postsPerPage,
-      allPosts,
-      posts
-    } = this.state;
-
+  const loadMorePosts = () => {
     const nextPage = page + postsPerPage;
     const nextsPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-
     posts.push(...nextsPosts);
-
-    this.setState({ posts, page: nextPage });
-
+    setPosts(posts);
+    setPage(nextPage);
   }
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { value } = e.target;
-    this.setState({ searchValue: value });
+    setSearchValue(value);
   }
 
-  render(){
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length; //Desativar botao se tiver acabado os posts
+  return (
+    <section className="container">
 
-    
-    const filterPosts = !!searchValue 
-      ? 
-      allPosts.filter(post => { //Filtra todos os posts
-         //Transforma todos os titulos em letra minuscula, includes incluir os searchValue em letra minusculas
-        return post.title.toLowerCase().includes(searchValue.toLowerCase());
-      })
-      : 
-      posts
-    ; 
+      <p>{new Date().toLocaleString('pt-br', { year: 'numeric', month: '2-digit', day: '2-digit', hour: 'numeric', minute: 'numeric' })}</p>
 
-    return (
-      <section className="container">
+      <div className="search-container">
+        {
+          !!searchValue //Pega valor do input ao digitar
+          &&
+          <h1>Pesquisa: { searchValue }</h1>
+        }
 
-        <div className="search-container">
-          {
-            !!searchValue //Pega valor do input ao digitar
-            &&
-            <h1>Pesquisa: { searchValue }</h1>
-          }
+        <TextInput 
+          handleChange={ handleChange }
+          searchValue={ searchValue }
+        />
+      </div>
+      
+      {filterPosts.length > 0 && ( //Se existir na pesquisa algum post = mostra os posts
+        <Posts posts={ filterPosts } />
+      )}      
 
-          <TextInput 
-            handleChange={ this.handleChange }
-            searchValue={ searchValue }
-          />
-        </div>
-        
-        {filterPosts.length > 0 && ( //Se existir na pesquisa algum post = mostra os posts
-          <Posts posts={ filterPosts } />
-        )}      
+      {filterPosts.length === 0 && ( //Se não existir na pesquisa algum post = mostra paragrafo
+        <p>Não existi nenhum post com esse titulo</p>
+      )}   
+      
+      {!searchValue  //Desaparecer botão ao digitar no input
+      && (
+          <Button 
+          text="Carregar mais posts" 
+          handleFunction={ loadMorePosts } 
+          disabled={ noMorePosts }
+        />
+      )}        
 
-        {filterPosts.length === 0 && ( //Se não existir na pesquisa algum post = mostra paragrafo
-          <p>Não existi nenhum post com esse titulo</p>
-        )}   
-        
-        {!searchValue  //Desaparecer botão ao digitar no input
-        && (
-            <Button 
-            text="Carregar mais posts" 
-            handleFunction={ this.loadMorePosts } 
-            disabled={ noMorePosts }
-          />
-        )}        
-
-      </section>
-    );
-  }
+    </section>
+  );
 }
-
-export default Home;
